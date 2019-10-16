afdc16b1daf46f0b681a55398f31d75e54cf68e5
Lakshmi-Sunil <lakshmisunil12@gmail.com>
2019-10-16 21:25:13 +0530

    Insertion sort

--- insertionsort.c
+++ insertionsort.c
@@ -16,17 +16,16 @@ int main()
 	{
 		printf("%d\t",a[i]);
 	}
-	for(i=0;i<n;i++)
+	for(i=1;i<n;i++)
 	{
-		for(j=i+1;j>0;j--)
+		temp=a[i];
+		j=i-1;
+		while(a[j]>temp && j>=0)
 		{
-			if(a[j-1]>a[j])
-			{
-				temp=a[j-1];
-				a[j-1]=a[j];
-				a[j]=temp;
-			}
+			a[j+1] = a[j];
+			j=j-1;
 		}
+		a[j+1] = temp;
 	}
 	printf("\nSorted array is:\n");
 	for(i=0;i<n;i++)
